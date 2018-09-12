a230fd3e403e2f055c2ef01adb94c16fb0729a2d
hapev <league@WorthyMini-6.attlocal.net>
2018-09-11 18:22:09 -0700

    asdasdasdasda

--- src/_00_Sorting_Algorithms/BubbleSorter.java
+++ src/_00_Sorting_Algorithms/BubbleSorter.java
@@ -1,16 +1,30 @@
 package _00_Sorting_Algorithms;
 
-public class BubbleSorter extends Sorter{
+public class BubbleSorter extends Sorter {
 	public BubbleSorter() {
 		type = "Bubble";
 	}
-	
-	//1. Use the bubble sorting algorithm to sort the array.
-	//   You can use display.updateDisplay() to show the current
-	//   progress on the graph.
+
+	// 1. Use the bubble sorting algorithm to sort the array.
+	// You can use display.updateDisplay() to show the current
+	// progress on the graph.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-		
+		boolean changed = false;
+		do {
+			changed = false;
+			for (int i = 1; i < array.length; i++) {
+				if (array[i - 1] > array[i]) {
+					changed = true;
+					int a = array[i];
+					array[i] = array[i - 1];
+					array[i - 1] = a;
+					display.updateDisplay();
+				}
+
+			}
+
+		} while (changed == true);
 	}
-	
+
 }
