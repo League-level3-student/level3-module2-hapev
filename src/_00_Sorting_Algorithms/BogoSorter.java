0159060bd3c6f518ee435512e005c31f6bf1c1c6
hapev <league@WTS14-2.attlocal.net>
2018-09-25 18:29:09 -0700

    finished level 3

--- src/_00_Sorting_Algorithms/BogoSorter.java
+++ src/_00_Sorting_Algorithms/BogoSorter.java
@@ -6,20 +6,42 @@ public class BogoSorter extends Sorter {
 	public BogoSorter() {
 		type = "Bogo";
 	}
-	
-	// Bogo sort is a joke sorting algorithm. It is considered the most 
+
+	// Bogo sort is a joke sorting algorithm. It is considered the most
 	// inefficient sorting algorithm while still maintaining the possibility
 	// of eventually sorting data.
-	
+
 	// It works by following these steps:
 	// STEP 1. Is the array in order?
-	//	if yes, finished; if no, go to step 2.
+	// if yes, finished; if no, go to step 2.
 	// STEP 2. Take two random elements in the array and swap them.
 	// STEP 3. Go back to step 1.
-	
-	//1. Complete the sort method using the Bogo sort algorithm. 
+
+	// 1. Complete the sort method using the Bogo sort algorithm.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
+
+		Random r = new Random();
+		while (!isSorted(array)) {
+			int random1 = r.nextInt(array.length);
+			int random2 = r.nextInt(array.length);
+			int swapper = array[random2];
+			array[random2] = array[random1];
+			array[random1] = swapper;
+			display.updateDisplay();
+
+		}
+	}
+
+	boolean isSorted(int[] array) {
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < array[i - 1]) {
+				return false;
+
+			}
+		}
+
+		return true;
 
 	}
 }
